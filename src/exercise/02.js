2ff85bba7592694fbb3eb99156c1be26ad140f84
Nicole Furlan <nfurlan@alley.co>
2021-07-27 15:11:50 -0400

    Removing unneeded value property on <input />

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -17,7 +17,7 @@ function Greeting({initialName = ''}) {
     <div>
       <form>
         <label htmlFor="name">Name: </label>
-        <input value={name} onChange={handleChange} id="name" />
+        <input onChange={handleChange} id="name" />
       </form>
       {name ? <strong>Hello {name}</strong> : 'Please type your name'}
     </div>
